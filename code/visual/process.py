29d1a292953ac45c36538cfeeaeca6198369a090
Yiluo Li <yiluo.li.astrophysics@gmail.com>
2019-05-14 21:31:35 -0700

    fixed dimension

--- code/visual/process.py
+++ code/visual/process.py
@@ -1,26 +1,45 @@
 import numpy as np
 import serial
 import matplotlib.pyplot as plt
+import lib.train as tr
 
 k, modem, baud = input("k, modem, baud:\n").split()
-train = Train(int(k), modem, baud)
+train = tr.Train(int(k), modem, baud)
 theta = train.start()
+ini_val = []
+ini_val_recorded = False
 
 ser = serial.Serial('/dev/cu.usbmodem'+modem, int(baud))
 
 plt.figure()
 plt.xlim(0, 30.1)
 plt.ylim(0, 29.8)
-scat = plt.scatter(0, 0, c='b', s=10, marker='.')
+scat = plt.scatter(1, 1, c='b', s=20, marker='.')
 
 while True:
-    readIn = ser.readline().decode('utf-8').split()
-    readIn = np.array(list(map(float, readIn)))
-    readIn = list(map(lambda x: np.sqrt(np.mean(np.array(x)**2)), list(readIn.transpose())))
+    if not ini_val_recorded:
+        try:
+            readIn = ser.readline().decode('utf-8').split()
+        except UnicodeDecodeError:
+            continue
+        readIn = np.array(list(map(float, readIn)))
+        readIn = readIn.reshape((40, 32))
+        readIn = list(map(lambda x: np.sqrt(np.mean(np.arrau(x)**2)), list(readIn.transpose())))
+        readIn.insert(0, 1)
+        init_val_recorded = True
+    else:
+        try:
+            readIn = ser.readline().decode('utf-8').split()
+        except UnicodeDecodeError:
+            continue
+        readIn = np.array(list(map(float, readIn)))
+        readIn = readIn.reshape((40, 32)) # 40 measurements, 32 features
+        readIn = list(map(lambda x: np.sqrt(np.mean(np.array(x)**2)), list(readIn.transpose())))
+        readIn.insert(0, 1)
 
-    Y = np.array(readIn) * theta
-
-    scat.set_offsets(Y)
-    plt.pause(1e-10)
+        Y = np.array(readIn) * theta
+        print(Y)
+        scat.set_offsets(Y)
+        plt.pause(1e-10)
 
 plt.show()
